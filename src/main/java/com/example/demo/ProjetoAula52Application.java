8eddea49bf3163d01c88bf597fae4906a4030e68
Islane dos Santos Silva <p000islane.silva@prservicos.com.br>
2019-08-16 17:08:34 -0300

    Proj de um CRUD básico com Java Spring Framework

---
+++ src/main/java/com/example/demo/ProjetoAula52Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAula52Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAula52Application.class, args);
+	}
+
+}
